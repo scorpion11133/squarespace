e0c41d3df52fcf68a11d8c8812a5ce7a3d274e07
Heorhii Vereshchaha <heorhii.vereshchaha@computools.com>
2022-09-07 14:49:21 +0300

    init

---
+++ index.js
@@ -0,0 +1,97 @@
+import express from 'express';
+import fetch from 'node-fetch';
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+
+app.use(cors({ origin: '*' }));
+
+app.get('/api/v1/courses', async (req, res) => {
+  const options = {
+    method: 'GET',
+    headers: {
+      '/**': {
+        'Access-Control-Allow-Origin': '*',
+      },
+      Accept: 'application/json',
+      apiKey: process.env.API_KEY,
+    },
+  };
+
+  const response = await fetch(
+    'https://developers.teachable.com/v1/courses',
+    options
+  );
+  const courses = await response.json();
+
+  return res.json(courses);
+});
+
+app.get('/api/v1/courses/1335944/enrollments', async (req, res) => {
+  const options = {
+    method: 'GET',
+    headers: {
+      '/**': {
+        'Access-Control-Allow-Origin': '*',
+      },
+      Accept: 'application/json',
+      apiKey: process.env.API_KEY,
+    },
+  };
+
+  const response = await fetch(
+    'https://developers.teachable.com/v1/courses/1335944/enrollments',
+    options
+  );
+  const enrollments = await response.json();
+
+  return res.json(enrollments);
+});
+
+app.get('/api/v1/courses/1335944', async (req, res) => {
+  const options = {
+    method: 'GET',
+    headers: {
+      '/**': {
+        'Access-Control-Allow-Origin': '*',
+      },
+      Accept: 'application/json',
+      apiKey: process.env.API_KEY,
+    },
+  };
+
+  const response = await fetch(
+    'https://developers.teachable.com/v1/courses/1335944',
+    options
+  );
+  const webinars = await response.json();
+
+  return res.json(webinars);
+});
+
+app.get('/api/v1/users', async (req, res) => {
+  const options = {
+    method: 'GET',
+    headers: {
+      '/**': {
+        'Access-Control-Allow-Origin': '*',
+      },
+      Accept: 'application/json',
+      apiKey: process.env.API_KEY,
+    },
+  };
+
+  const response = await fetch(
+    'https://developers.teachable.com/v1/users',
+    options
+  );
+  const users = await response.json();
+
+  return res.json(users);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
